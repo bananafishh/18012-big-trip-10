b9a082eb82d9c66b1e188971352973e6acd395c6
z.dyshekova <z.dyshekova@fun-box.ru>
2019-12-03 04:44:21 +0300

    Отрисовка меню приложения.

---
+++ src/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const createMenuTemplate = () => {
+  return (
+    `<nav class="trip-controls__trip-tabs  trip-tabs">
+      <a class="trip-tabs__btn  trip-tabs__btn--active" href="#">Table</a>
+      <a class="trip-tabs__btn" href="#">Stats</a>
+    </nav>`
+  );
+};
+
+const render = (container, template, place = `beforeend`) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+const tripControlsElement = document.querySelector(`.trip-controls`);
+
+const menuTitleElement = tripControlsElement.querySelector(`.visually-hidden`);
+render(menuTitleElement, createMenuTemplate(), `afterend`);
