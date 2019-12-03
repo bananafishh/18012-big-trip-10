3044d1f8d9c9ef9a6f6aef01d796ea04e7f3feb8
z.dyshekova <z.dyshekova@fun-box.ru>
2019-12-03 04:17:57 +0300

    Отрисовка точек маршрута.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const TRIP_DAYS_AMOUNT = 3;
+const TRIP_EVENTS_AMOUNT = 4;
+
 const createMenuTemplate = () => {
   return (
     `<nav class="trip-controls__trip-tabs  trip-tabs">
@@ -257,6 +260,60 @@ const createEditTripEventTemplate = () => {
   );
 };
 
+const createTripDaysTemplate = () => `<ul class="trip-days"></ul>`;
+
+const createTripDayTemplate = () => {
+  return (
+    `<li class="trip-days__item  day">
+      <div class="day__info">
+        <span class="day__counter">1</span>
+        <time class="day__date" datetime="2019-03-18">MAR 18</time>
+      </div>
+  
+      <ul class="trip-events__list"></ul>
+    </li>`
+  );
+};
+
+const createTripEventTemplate = () => {
+  return (
+    `<li class="trip-events__item">
+      <div class="event">
+        <div class="event__type">
+          <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+        </div>
+        <h3 class="event__title">Taxi to airport</h3>
+  
+        <div class="event__schedule">
+          <p class="event__time">
+            <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+            &mdash;
+            <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+          </p>
+          <p class="event__duration">1H 30M</p>
+        </div>
+  
+        <p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">20</span>
+        </p>
+  
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">
+          <li class="event__offer">
+            <span class="event__offer-title">Order Uber</span>
+            &plus;
+            &euro;&nbsp;<span class="event__offer-price">20</span>
+           </li>
+        </ul>
+  
+        <button class="event__rollup-btn" type="button">
+          <span class="visually-hidden">Open event</span>
+        </button>
+      </div>
+    </li>`
+  );
+};
+
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
 };
@@ -273,3 +330,18 @@ render(tripInfoElement, createTripInfoTemplate(), `afterbegin`);
 const tripEventsElement = document.querySelector(`.trip-events`);
 render(tripEventsElement, createSortTemplate());
 render(tripEventsElement, createEditTripEventTemplate());
+render(tripEventsElement, createTripDaysTemplate());
+
+const tripDaysElement = document.querySelector(`.trip-days`);
+new Array(TRIP_DAYS_AMOUNT)
+  .fill(``)
+  .forEach((day, dayIndex) => {
+    render(tripDaysElement, createTripDayTemplate());
+
+    const tripDaysElements = document.querySelectorAll(`.day`);
+    const tripEventsListElement = tripDaysElements[dayIndex].querySelector(`.trip-events__list`);
+
+    new Array(TRIP_EVENTS_AMOUNT)
+      .fill(``)
+      .forEach(() => render(tripEventsListElement, createTripEventTemplate()));
+  });
